744106e347b725bc898782cb6643259a2d24c752
qapquiz <arm.armariya@gmail.com>
2023-12-24 16:42:33 +0700

    feat: add lspconfig, add lua_ls server

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,19 +1,40 @@
+local treesitter_options = {
+	ensure_installed = { 'lua' },
+	sync_install = false,
+	highlight = { enable = true },
+	indent = { enable = true },  
+}
+
+local mason_options = {
+	ensure_installed = { "lua_ls" },
+}
+
 return {
 	-- treesitter
 	{
 		'nvim-treesitter/nvim-treesitter',
 		build = ':TSUpdate',
 		config = function () 
-		  local configs = require('nvim-treesitter.configs')
+			local configs = require('nvim-treesitter.configs')
 
-		  configs.setup({
-			  ensure_installed = { 'lua' },
-			  sync_install = false,
-			  highlight = { enable = true },
-			  indent = { enable = true },  
-			})
+			configs.setup(treesitter_options)
 		end
 	},
 
 	-- lspconfig
+	{ 'williamboman/mason.nvim', config = true },
+	{
+		'williamboman/mason-lspconfig.nvim',
+		opts = mason_options,
+	},
+	{
+		'neovim/nvim-lspconfig',
+		config = function()
+			local lspconfig = require('lspconfig')
+
+			for _, lsp in ipairs(mason_options.ensure_installed) do
+				lspconfig[lsp].setup({})
+			end
+		end,
+	}
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,5 +3,7 @@ vim.cmd('set tabstop=4')
 vim.cmd('set softtabstop=4')
 vim.cmd('set shiftwidth=4')
 
+vim.cmd('set relativenumber')
+
 vim.g.mapleader = ' '
 
