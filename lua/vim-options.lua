90535637af4af04f49cad1190093284ad76429a4
qapquiz <arm.armariya@gmail.com>
2023-12-24 20:59:53 +0700

    chore: add description for keymap

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,6 +8,21 @@ vim.cmd('set relativenumber')
 vim.g.mapleader = ' '
 
 -- mappings
-vim.keymap.set('n', '<TAB>', '<cmd>bnext<cr>')
-vim.keymap.set('n', '<S-TAB>', '<cmd>bprevious<cr>')
+local map = vim.keymap.set
+
+map('n', '<TAB>', '<cmd>bnext<cr>', { desc = 'Go to next buffer' })
+map('n', '<S-TAB>', '<cmd>bprevious<cr>', { desc = 'Go to previes buffer' })
+map('n', '<leader>x', '<cmd>bdelete<cr>', { des = 'Delete Buffer' })
+
+-- Window navigation
+map('n', '<C-H>', '<C-w>h', { desc = 'Focus on left window' })
+map('n', '<C-J>', '<C-w>j', { desc = 'Focus on below window' })
+map('n', '<C-K>', '<C-w>k', { desc = 'Focus on above window' })
+map('n', '<C-L>', '<C-w>l', { desc = 'Focus on right window' })
+
+-- Window resize (respecting `v:count`)
+map('n', '<M-Left>',  '"<Cmd>vertical resize -" . v:count1 . "<CR>"', { expr = true, replace_keycodes = false, desc = 'Decrease window width' })
+map('n', '<M-Down>',  '"<Cmd>resize -"          . v:count1 . "<CR>"', { expr = true, replace_keycodes = false, desc = 'Decrease window height' })
+map('n', '<M-Up>',    '"<Cmd>resize +"          . v:count1 . "<CR>"', { expr = true, replace_keycodes = false, desc = 'Increase window height' })
+map('n', '<M-Right>', '"<Cmd>vertical resize +" . v:count1 . "<CR>"', { expr = true, replace_keycodes = false, desc = 'Increase window width' })
 
