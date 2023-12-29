22ec112151bd7c99f23d16e250a7b15572b2ff52
qapquiz <arm.armariya@gmail.com>
2023-12-29 21:41:43 +0700

    feat: add nvim-lint and luasnip

---
+++ lua/plugins/linter.lua
@@ -0,0 +1,38 @@
+local linters = { 'luacheck' }
+
+return {
+	{
+		'mfussenegger/nvim-lint',
+		event = { 'VeryLazy' },
+		opts = {
+			-- Events to trigger linter
+			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+			linters_by_ft = {
+				lua = { 'luacheck' },
+			},
+		},
+		config = function(_, opts)
+			local mason_registry = require('mason-registry')
+			for _, linter in ipairs(linters) do
+				if not mason_registry.is_installed(linter) then
+					vim.cmd('MasonInstall ' .. linter)
+				end
+			end
+
+			local lint = require('lint')
+			lint.linters_by_ft = opts.linters_by_ft
+
+			vim.api.nvim_create_autocmd(
+				{ 'BufWritePost' },
+				{
+					callback = function()
+						lint.try_lint()
+					end
+				}
+			)
+		end,
+		dependencies = {
+			{ 'williamboman/mason.nvim' }
+		}
+	},
+}
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -131,6 +131,34 @@ return {
 
 	-- auto completion
 	{
+		"L3MON4D3/LuaSnip",
+		build = (not jit.os:find("Windows"))
+			and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+			or nil,
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
+		-- stylua: ignore
+		keys = {
+			{
+				"<tab>",
+				function()
+					return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+				end,
+				expr = true, silent = true, mode = "i",
+			},
+			{ "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+			{ "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+		},
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		version = false, -- last release is way too old
 		event = "InsertEnter",
@@ -154,8 +182,8 @@ return {
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<TAB>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<S-TAB>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
