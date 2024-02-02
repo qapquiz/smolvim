8007505a074ebef9867c35689eb6b7f14292e794
s99344 <ariya.lawanitchanon@scb.co.th>
2024-02-02 13:54:57 +0700

    feat: change codeium cycle completions key

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -4,10 +4,10 @@ return {
 		event = 'BufEnter',
 		config = function()
 			vim.g.codeium_disable_bindings = 1
-			vim.keymap.set('i', '<M-l>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-			vim.keymap.set('i', '<TAB>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-			vim.keymap.set('i', '<S-TAB>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-			vim.keymap.set('i', '<M-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+			vim.keymap.set('i', '<M-l>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+			vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+			vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+			vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
 		end
 	},
 }
