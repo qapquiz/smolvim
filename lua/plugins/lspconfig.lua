61dda161a36bad3deb5d90fb6f799f1bfb534918
qapquiz <arm.armariya@gmail.com>
2024-03-16 17:20:00 +0700

    feat: use config for rustaceanvim from folke/lazyvim

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -118,6 +118,55 @@ return {
 		'mrcjkb/rustaceanvim',
 		version = '^4',
 		ft = { 'rust' },
+		opts = {
+			server = {
+				on_attach = function (_, bufnr)
+					vim.keymap.set(
+						'n',
+						'<leader>ca',
+						function()
+							vim.cmd.RustLsp('codeAction')
+						end,
+						{ desc = "Code Action", buffer = bufnr }
+					)
+					-- vim.keymap.set(
+					-- 	'n',
+					-- 	'<leader>dr',
+					-- 	function()
+					-- 		vim.cmd.RustLsp('debuggables')
+					-- 	end,
+					-- 	{ desc = "Rust debuggables", buffer = bufnr }
+					-- )
+				end,
+
+				default_settings = {
+					['rust-analyzer'] = {
+						cargo = {
+							allFeatures = true,
+							loadOutDirsFromCheck = true,
+							runBuildScripts = true,
+						},
+						-- Add clippy lints for Rust.
+						checkOnSave = {
+							allFeatures = true,
+							command = "clippy",
+							extraArgs = { "--no-deps" },
+						},
+						procMacro = {
+							enable = true,
+							ignored = {
+								["async-trait"] = { "async_trait" },
+								["napi-derive"] = { "napi" },
+								["async-recursion"] = { "async_recursion" },
+							},
+						},
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {})
+		end,
 	},
 
 	-- formatter
